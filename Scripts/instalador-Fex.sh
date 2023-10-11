bcb2d4df42d550e27e3d248604bccc003368b926
gapereyra <gapereyra@siu.edu.ar>
2023-10-10 21:54:38 -0300

    TESTING 4

--- Scripts/instalador-Fex.sh
+++ Scripts/instalador-Fex.sh
@@ -133,12 +133,12 @@ lines=("INSTALLING PACKAGES"
 
 box_out_warning "${lines[@]}"
 
-if [ $opcao = 1 ]
+if [ $option = 1 ]
 then
 
        pkg install pulseaudio wget tsu xkeyboard-config wget -y & &>/dev/null
 
-elif [ $opcao = 2 ]
+elif [ $option = 2 ]
 then
        pkg install pulseaudio wget xkeyboard-config proot-distro wget -y & &>/dev/null  
 
@@ -173,311 +173,161 @@ lines=("TERMUX:X11.DEB INSTALLED"
        ""
        "[CTRL+C] EXIT")
 
-box_out_warning "${lines[@]}"
+box_out_success "${lines[@]}"
 
 sleep 2
-#echo "==============================="
-#echo "=                             ="
-#echo "=  TERMUX:X11.DEB INSTALADO   ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#sleep 1
-#
-#clear
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  CRIANDO DIRETORIO FEX      ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#sleep 1
-#
-#clear 
-#
-#if [ -f $PREFIX/bin/Fex ]
-#then
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  DIRETORIO PREFIX JA EXISTE ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#rm $PREFIX/bin/Fex
-#
-#sleep 1
-#
-#else
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  DIRETORIO PREFIX CRIADO    ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#sleep 1
-#
-#fi
-#
-#clear
-#
-#if [ -d /storage/Fex ]
-#then
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  DIRETORIO FEX JA EXISTE    ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#sleep 1
-#
-#else
-#
-#mkdir /storage/Fex
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  DIRETORIO FEX CRIADO  ✓    ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#sleep 1
-#
-#fi
-#
-#clear
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  BAIXANDO ROOTFS UBUNTU     ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#if [ $opcao = 1 ]
-#then
-#        wget -q https://github.com/Ilya114/Box64Droid/releases/download/chroot/box64droid-rootfs-beta-chroot.tar.xz
-#
-#elif [ $opcao = 2 ]
-#then
-#        mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/ &>/dev/null
-#	mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ &>/dev/null
-#	mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu &>/dev/null
-#	wget -q https://github.com/Ilya114/Box64Droid/releases/download/beta/box64droid-rootfs-beta.tar.xz 
-#fi
-#
-#clear
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  ROOTFS UBUNTU BAIXADO      ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#sleep 1
-#
-#clear
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  INSTALANDO UBUNTU ROOTFS   ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#if [ $opcao = 1 ]
-#then
-#       sudo mkdir ~/Fex
-#       sudo tar -xJf box64droid-rootfs-beta-chroot.tar.xz -C ~/Fex &>/dev/null
-#elif [ $opcao = 2 ]
-#then
-#       proot-distro restore box64droid-rootfs-beta.tar.xz &>/dev/null
-#fi
-#
-#sleep 1
-#
-#clear
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  FINALIZANDO ROOTFS         ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#if [ $opcao = 1 ]
-#then
-#
-#cd Fex
-#
-#sudo curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/checkconfig && sudo chmod +x checkconfig
-#sudo curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/Fexconfig && sudo chmod+x Fexconfig
-#sudo curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/InstallFex && sudo chmod +x InstallFex
-#sudo curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/Fex && sudo chmod +x Fex
-#sudo curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/FexMore && sudo chmod +x FexMore
-#
-#sudo mv FexMore InstallFex Fex checkconfig Fexconfig /data/data/com.termux/files/home/Fex/opt
-#
-#sudo curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/start-fex && sudo chmod +x start-fex
-#
-#sudo mv start-fex /data/data/com.termux/files/home/Fex/opt/Scripts
-#
-#sudo rm -f /data/data/com.termux/files/home/Fex/opt/start
-#
-#sudo rm -f /data/data/com.termux/files/home/Fex/opt/Box64Droid.conf
-#
-#sudo rm -f /data/data/com.termux/files/home/Fex/opt/Scripts/start-box64
-#
-#cd $HOME
-#
-#elif [ $opcao = 2 ]
-#then
-#
-#cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/opt
-#
-#rm -f checkconfig start
-#
-#curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/checkconfig && chmod +x checkconfig
-#curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/Fexconfig && chmod+x Fexconfig
-#curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/InstallFex && chmod +x InstallFex
-#curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/Fex && chmod +x Fex
-#curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/FexMore && chmod +x FexMore
-#
-#cd Scripts
-#
-#rm -f start-box64
-#
-#curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Rootfs/Ubuntu/opt/start-fex && chmod +x start-fex
-#
-#cd $HOME
-#
-#fi
-#
-#sleep 1
-#
-#clear
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  UBUNTU ROOTFS INSTALADO    ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#sleep 1
-#
-#clear
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  CRIANDO INICIAVEL          ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#curl -O https://raw.githubusercontent.com/OpomboMORTO/FEX-android/main/Scripts/fex &>/dev/null
-#chmod +x fex
-#mv fex $PREFIX/bin &>/dev/null
-#
-#sleep 1
-#
-#clear
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  LIMPANDO ARQUIVOS          ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
-#rm -rf instalador-Fex.sh &>/dev/null
-#
-#if [ $opcao = 1 ]
-#then
-#
-#rm box64droid-rootfs-beta-chroot.tar.xz &>/dev/null
-#
-#elif [ $opcao = 2 ]
-#then
-#
-#rm box64droid-rootfs-beta.tar.xz &>/dev/null
-#
-#fi
-#
-#sleep 2
-#
-#clear
-#
-#echo "==============================="
-#echo "=                             ="
-#echo "=  CONCLUIDO A INSTALACÃO     ="
-#echo "=                             ="
-#echo "=  [CRT + C] PARA CANCELAR    ="
-#echo "=                             ="
-#echo "=  AGUARDE .                  ="
-#echo "=                             ="
-#echo "=                             ="
-#echo "==============================="
-#
+clear
+
+lines=("CREATING FEXDROID SYSTEM"
+       "PLEASE WAIT"
+       ""
+       "[CTRL+C] EXIT")
+
+box_out_warning "${lines[@]}"
+
+if [ -f $PREFIX/bin/Fex ]
+then
+
+  lines=("FEXDROID PREFIX ALREADY EXISTS"
+         ""
+         "[CTRL+C] EXIT")
+
+  box_out_warning "${lines[@]}"
+
+  rm $PREFIX/bin/Fex
+
+  sleep 1
+
+fi
+clear
+
+if [ -d /storage/Fex ]
+then
+
+  lines=("FEXDROID FOLDER ALREADY EXISTS"
+         ""
+         "[CTRL+C] EXIT")
+
+  box_out_warning "${lines[@]}"
+
+  sleep 1
+
+else
+
+  mkdir /storage/Fex
+
+  lines=("FEXDROID SYSTEM INSTALLED"
+         ""
+         "[CTRL+C] EXIT")
+
+  box_out_success "${lines[@]}"
+
+  sleep 1
+
+fi
+
+clear
+
+lines=("INSTALLING UBUNTU-ROOTFS"
+       "PLEASE WAIT"
+       ""
+       "[CTRL+C] EXIT")
+
+box_out_warning "${lines[@]}"
+
+if [ $option = 1 ]
+then
+  wget -q https://github.com/Ilya114/Box64Droid/releases/download/stable/box64droid-rootfs-chroot.tar.xz
+  sudo mkdir ~/Fex
+  sudo tar -xJf box64droid-rootfs-beta-chroot.tar.xz -C ~/Fex &>/dev/null
+  cd Fex
+  sudo curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/checkconfig && sudo chmod +x checkconfig
+  sudo curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/Fexconfig && sudo chmod+x Fexconfig
+  sudo curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/InstallFex && sudo chmod +x InstallFex
+  sudo curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/Fex && sudo chmod +x Fex
+  sudo curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/FexMore && sudo chmod +x FexMore
+  sudo mv FexMore InstallFex Fex checkconfig Fexconfig /data/data/com.termux/files/home/Fex/opt
+  sudo curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/start-fex && sudo chmod +x start-fex
+  sudo mv start-fex /data/data/com.termux/files/home/Fex/opt/Scripts
+  sudo rm -f /data/data/com.termux/files/home/Fex/opt/start
+  sudo rm -f /data/data/com.termux/files/home/Fex/opt/Box64Droid.conf
+  sudo rm -f /data/data/com.termux/files/home/Fex/opt/Scripts/start-box64
+  cd $HOME
+
+elif [ $option = 2 ]
+then
+  mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/ &>/dev/null
+	mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ &>/dev/null
+	mkdir -p /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu &>/dev/null
+	wget -q https://github.com/Ilya114/Box64Droid/releases/download/stable/box64droid-rootfs.tar.xz
+  proot-distro restore box64droid-rootfs-beta.tar.xz &>/dev/null
+  cd /data/data/com.termux/files/usr/var/lib/proot-distro/installed-rootfs/ubuntu/opt
+  rm -f checkconfig start
+  curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/checkconfig && chmod +x checkconfig
+  curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/Fexconfig && chmod+x Fexconfig
+  curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/InstallFex && chmod +x InstallFex
+  curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/Fex && chmod +x Fex
+  curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/FexMore && chmod +x FexMore
+  cd Scripts
+  rm -f start-box64
+  curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Rootfs/Ubuntu/opt/start-fex && chmod +x start-fex
+  cd $HOME
+fi
+
+sleep 1
+
+clear
+
+lines=("UBUNTU-ROOTFS INSTALLED"
+       ""
+       "[CTRL+C] EXIT")
+
+box_out_success "${lines[@]}"
+
+sleep 1
+
+clear
+
+lines=("CREATING LAUNCHER"
+       "PLEASE WAIT"
+       ""
+       "[CTRL+C] EXIT")
+
+box_out_warning "${lines[@]}"
+
+curl -O https://raw.githubusercontent.com/GabiAle97/FexDroid/main/Scripts/fex &>/dev/null
+chmod +x fex
+mv fex $PREFIX/bin &>/dev/null
+
+sleep 1
+
+clear
+
+lines=("DELETING JUNK"
+       "PLEASE WAIT"
+       ""
+       "[CTRL+C] EXIT")
+
+box_out_warning "${lines[@]}"
+
+rm -rf instalador-Fex.sh &>/dev/null
+
+if [ $option = 1 ]
+then
+
+  rm box64droid-rootfs-beta-chroot.tar.xz &>/dev/null
+
+elif [ $option = 2 ]
+then
+
+  rm box64droid-rootfs-beta.tar.xz &>/dev/null
+
+fi
+
+sleep 2
+
+clear
+
+lines=("INSTALLATION FINISHED")
+
+box_out_warning "${lines[@]}"
